4dd16ff1c8fe483808749f35864ab144274638ee
Sourabh Mourya <sourabh@immersiveinfotech.com>
2024-10-24 11:33:55 +0530

    fix errors

--- src/components/Header/CartDropdown.tsx
+++ src/components/Header/CartDropdown.tsx
@@ -11,14 +11,14 @@ import Link from "next/link";
 
 export default function CartDropdown() {
   const renderProduct = (item: Product, index: number, close: () => void) => {
-    const { name, price, image } = item;
+    const { title, price, thumbnail } = item;
     return (
       <div key={index} className="flex py-5 last:pb-0">
         <div className="relative h-24 w-20 flex-shrink-0 overflow-hidden rounded-xl bg-slate-100">
           <Image
             fill
-            src={image}
-            alt={name}
+            src={thumbnail}
+            alt={title}
             className="h-full w-full object-contain object-center"
           />
           <Link
@@ -34,7 +34,7 @@ export default function CartDropdown() {
               <div>
                 <h3 className="text-base font-medium ">
                   <Link onClick={close} href={"/product-detail"}>
-                    {name}
+                    {title}
                   </Link>
                 </h3>
                 <p className="mt-1 text-sm text-slate-500 dark:text-slate-400">
