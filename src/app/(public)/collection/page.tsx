008b4faa630cb10dd7f381ef1c3224ce6189b6ad
sourabh-immersive <sourabh@immersiveinfotech.com>
2025-01-15 15:34:14 +0530

    for review on production

--- src/app/(public)/collection/page.tsx
+++ src/app/(public)/collection/page.tsx
@@ -32,7 +32,7 @@ const PageCollection = async ({}) => {
 
             {/* LOOP ITEMS */}
             <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-8 gap-y-10 mt-8 lg:mt-10">
-              {fetchedProducts.map((item: any, index: any) => (
+              {fetchedProducts.data.map((item: any, index: any) => (
                 <ProductCard data={item} key={index} />
               ))}
             </div>
