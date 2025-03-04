c884ca0e57f9d208fcc31356a1a48d63a0ff9681
sourabh-immersive <sourabh@immersiveinfotech.com>
2025-03-04 18:30:45 +0530

    bug fixes

--- src/app/(public)/hair-test/page.tsx
+++ src/app/(public)/hair-test/page.tsx
@@ -799,7 +799,7 @@ const StepForm: React.FC = () => {
   const progress = (currentFieldPosition / totalFields) * 100;
 
   return (
-    <div className="hairTestRow bg-[url(/bg.jpg)] h-screen bg-cover bg-bottom bg-no-repeat">
+    <div className="hairTestRow bg-[url(/bg.jpg)] h-screen min-h-fit bg-cover bg-bottom bg-no-repeat">
       <div className="max-w-4xl mx-auto p-4 md:p-6 pt-0 pb-44 border border-r-0 border-l-0 border-b-0 border-gray-200 rounded-xl py-10">
         <h2 className="text-xl font-semibold text-center rounded-b-lg mb-6 flex justify-center bg-blue-600 text-white py-3 px-5 w-fit m-auto">
           Start Hair Test
